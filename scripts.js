df69723fc087c08edba2c7914d108013b7212b17
NaraeKwon <nali4214@gmail.com>
2019-06-14 23:49:12 -0700

    fixed minor errrors on nav menu items

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ const bodyTag = document.querySelector('body');
 const header = document.querySelector('header');
 const logoLetters = document.querySelectorAll('.letter');
 const navElems = document.querySelector('nav');
+const navElemsLinks = document.querySelectorAll('.nav-main-menu-item a')
 const signIn = document.querySelector('.sign-in');
 const subNavMenus = document.querySelectorAll('.nav-sub-menu');
 
@@ -16,12 +17,15 @@ addEventListener('scroll', () => {
 	header.style.setProperty('height', scrolled() > 5 ? "80px" : "140px");
 	header.style.boxShadow = scrolled() > 5 ? "0 0 5px rgba(0, 0, 0, 0.5)" : "none";
 	navElems.style.setProperty('color', scrolled() > 5 ? "#380089" : "#fff");
-	signIn.style.setProperty('color', scrolled() > 5 ? "#380089" : "#fff");
 	signIn.style.setProperty('background', scrolled() > 5 ? "rgba(225, 217, 237, 1)" : "rgba(225, 217, 237, 0.2)");
 	for (i = 0; i < logoLetters.length; i ++) {
 		logoLetters[i].style.setProperty('fill', scrolled() > 5 ? "#380089" : "#fff");
 	};
+	for (i = 0; i < navElemsLinks.length; i ++) {
+		navElemsLinks[i].style.setProperty('color', scrolled() > 5 ? "#380089" : "#fff");
+	};
 	for (i = 0; i < subNavMenus.length; i ++) {
 		subNavMenus[i].style.boxShadow = scrolled() > 5 ? "0 0 5px rgba(0, 0, 0, 0.5)" : "none";
-	}
+	};
+
 });
